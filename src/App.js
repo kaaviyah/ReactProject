3da75ea2ab72471312d33ca6faaf60d4418dcf99
kaaviyah <kaaviyah7@gmail.com>
2023-07-30 11:11:34 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import './App.css';
-import Login from './Pages/Login';
-import MainPage from './Pages/MainPage';
-
-
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Login from "./Pages/Login";
+import MainPage from "./Pages/MainPage";
 
 function App() {
-//  const username="Kaaviyah";
-      return(
-        <div>
-      <BrowserRouter>
-         <Routes>
-         
-          <Route path="/" element={<Login />}></Route> 
-          <Route path="/Mainpage"  element={<MainPage />}></Route> 
-          {/* <Route path="/Mainpage" render={()=> <MainPage username={username} />}/> */}
-          </Routes>
-          </BrowserRouter>
-
-        </div>
-      )
-
-  
-       
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />}></Route>
+          <Route path="/Mainpage" element={<MainPage />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
